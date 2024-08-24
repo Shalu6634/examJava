8c37b7379a7cb2cc97cacecebbb62b426d429e34
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-08-24 20:28:03 +0530

    Update Prime.java

--- exam/src/Prime.java
+++ exam/src/Prime.java
@@ -1,27 +1,20 @@
 import java.util.Scanner;
 
 public class Prime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+        int sum = 0;
+        System.out.print("Enter value: ");
+        size = scanner.nextInt();
 
-//WAP TO FIND THE SUM OF PRIME NUMBERS FROM 1 TO N.
-public static void main(String[] args) {
-    Scanner scanner = new  Scanner(System.in);
-    int size = scanner.nextInt();
-    int[] array = new int[size];
-
-    int a=0;
-    for(int i=0; i<array.length; i++)
-    {
-        for(int j=i+1; j<array.length; j++)
-        {
-            if(array[i]>array[j])
-            {
-                a = array[i];
-
+        for (int i = 1; i < size; i++) {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0 || i == 2 || i == 3) {
+                System.out.println(i);
+                sum += i;
             }
-
         }
+        System.out.println("Sum of Prime number: " + sum);
     }
-
-
 }
-}
+
